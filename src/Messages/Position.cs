e441b1ab98db2e810e08ea6c13be75634f0159b8
Futrime <futrime@outlook.com>
2023-03-07 10:32:03 +0800

    fix:(messages): fix protocol interface not implemented

--- src/Messages/Position.cs
+++ src/Messages/Position.cs
@@ -2,7 +2,7 @@ using System.Text.Json.Serialization;
 
 namespace NovelCraft.Sdk.Messages;
 
-internal record Position<T> {
+internal record Position<T> : IPosition<T> {
   [JsonPropertyName("x")]
   public required T X { get; init; }
 
