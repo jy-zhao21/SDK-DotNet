c78e0218636e50f85a0250e23ba968284c6642b9
Zhangyr2022 <zhangyr21@mails.tsinghua.edu.cn>
2023-03-07 10:15:09 +0800

    Update Client.cs

--- src/Client/Client.cs
+++ src/Client/Client.cs
@@ -1,15 +1,37 @@
+using System.Net.WebSockets;
 namespace NovelCraft.Sdk.Client;
 
 internal class Client : IClient {
+  private ClientWebSocket _clientWebSocket = new();
   public event EventHandler<NovelCraft.Sdk.Messages.IMessage>? MessageReceived;
+  /// <summary>Get buffer from a byte array</summary>
+  /// <param name="arr">byte array</param>
+  /// <returns> ArraySegment<byte> </returns>
+  private ArraySegment<byte> GetBuffer(byte[] array) {
+    return new ArraySegment<byte>(array);
+  }
+  /// <summary>Get buffer from a string</summary>
+  /// <param name="str">string</param>
+  /// <returns> ArraySegment<byte> </returns>
+  private ArraySegment<byte> GetBuffer(string str) {
+    return GetBuffer(System.Text.Encoding.UTF8.GetBytes(str));
+  }
 
 
-  public Client(string host, string port) {
-    throw new NotImplementedException();
+  public void Connect(Uri uri) {
+    Task task = _clientWebSocket.ConnectAsync(uri, CancellationToken.None);
+    task.Wait();
+    // Wait for connection to complete
+    while (!task.IsCompletedSuccessfully) {
+      task = _clientWebSocket.ConnectAsync(uri, CancellationToken.None);
+      task.Wait();
+    }
   }
 
 
   public void Send(NovelCraft.Sdk.Messages.IMessage message) {
-    throw new NotImplementedException();
+    if (this._clientWebSocket.State == WebSocketState.Connecting) {
+      this._clientWebSocket.SendAsync(this.GetBuffer(message.Json.ToJsonString()), WebSocketMessageType.Text, false, CancellationToken.None);
+    }
   }
 }
